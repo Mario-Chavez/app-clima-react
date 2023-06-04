7ef034293dbe2b76b4b1abe596962fe371452610
Mario-Chavez <marioachavez1@gmail.com>
2023-06-04 17:50:21 -0300

    se agrego muetra de error de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import Buscador from "./components/Buscador";
 import CardClima from "./components/CardClima.jsx";
 import TituloDeBusqueda from "./components/TituloDeBusqueda";
+import { XCircle } from "react-bootstrap-icons";
 
 const API_KEY = "a6058a0ce883d6e4b27adc6b93e15e01";
 
@@ -13,6 +14,7 @@ function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [lat, setLat] = useState("");
     const [long, setLong] = useState("");
+    const [errorDeFetch, setErrorDeFetch] = useState(false);
 
     useEffect(() => {}, [clima]);
 
@@ -27,8 +29,9 @@ function App() {
             setLong(dato.coord.lon);
             setClima([{ ...dato }]);
             setIsLoading(false);
+            setErrorDeFetch(false);
         } catch (error) {
-            console.log(error);
+            setErrorDeFetch(true);
         }
     };
 
@@ -41,11 +44,20 @@ function App() {
                 <TituloDeBusqueda />
                 {isLoading ? (
                     <div className="my-5 p-4 text-center">
-                        <Spinner
-                            animation="border"
-                            role="status"
-                            variant="primary"
-                        ></Spinner>
+                        {errorDeFetch ? (
+                            <div className="text-danger my-5 fs-5 ">
+                                <div className="my-4">
+                                    <XCircle size={50} />
+                                </div>
+                                Tu busqueda debe ser por CIUDADES y caracteres correctos
+                            </div>
+                        ) : (
+                            <Spinner
+                                animation="border"
+                                role="status"
+                                variant="primary"
+                            ></Spinner>
+                        )}
                     </div>
                 ) : (
                     <section className="row justify-content-evenly mt-5">
