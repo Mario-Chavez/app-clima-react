2437dce561d274a20d14f1a173892c79e432fe59
Mario-Chavez <marioachavez1@gmail.com>
2023-06-04 05:38:22 -0300

    se agrego estilo a la card del clima

--- src/components/CardClima.jsx
+++ src/components/CardClima.jsx
@@ -13,35 +13,38 @@ const CardClima = ({ clima, latitud, longitud }) => {
             ) : (
                 <div>
                     {clima.map((data, index) => (
-                        <Card key={index} className="bg border-5 pb-5 ">
+                        <Card key={index} className="bg border-5 pb-5 cardClima ">
                             <Card.Body>
-                                <Row>
-                                    <Col sm={12} xs={12} lg={4}>
-                                        <div>
-                                            <Card.Title>{data.name}</Card.Title>
-                                            <Card.Text>
+                                <Row className="my-5">
+                                    <Col sm={12} xs={12} lg={4} className="mb-4">
+                                        <div className="text-center">
+                                            <Card.Title className="fs-1">
+                                                {data.name}
+                                            </Card.Title>
+                                            <hr />
+                                            <Card.Text className="fw-bold">
                                                 Temperatura: {data.main.temp}°C
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 Sensación térmica: {data.main.feels_like}
                                                 °C
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 Humedad: {data.main.humidity}%
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 Presion: {data.main.pressure} hPa
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 Temperatura Max: {data.main.temp_max}°C
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 Temperatura Max: {data.main.temp_min}°C
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 {data.weather[0].description}
                                             </Card.Text>
-                                            <Card.Text>
+                                            <Card.Text className="fw-bold">
                                                 Velocidad del viento: {data.wind.speed}{" "}
                                                 km/h
                                             </Card.Text>
